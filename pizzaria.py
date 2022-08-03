16f9ee51b37e688b4cbf079355b3a151d534f6dd
Marco Silveira <48794477+marcosilveira2@users.noreply.github.com>
2022-08-03 13:53:58 -0300

    Update pizzaria.py

--- pizzaria.py
+++ pizzaria.py
@@ -1,27 +1,26 @@
 class Pizza:
 
-
     def preco():
         pass
 
 
 class PizzaDaNonna(Pizza):
 
-
     def __init__(self, ingredientes):
         self.ingredientes = ingredientes
 
     def preco(self):
         valor = 0
-        for i,p in self.ingredientes.items():
+        for i, p in self.ingredientes.items():
             valor += self.ingredientes[i]
-        return(valor)
+        return (valor)
+
 
 class PizzaDecorator(Pizza):
-    
 
     def __init__(self, pizzadecorada):
         self.pizzadecorada = pizzadecorada
+
 
 class BordaRecheada(PizzaDecorator):
 
@@ -30,6 +29,7 @@ class BordaRecheada(PizzaDecorator):
     def preco(self):
         return self.pizzadecorada.preco() + 10
 
+
 class MassaIntegral(PizzaDecorator):
 
     """Adiciona Massa Integral"""
@@ -37,12 +37,14 @@ class MassaIntegral(PizzaDecorator):
     def preco(self):
         return self.pizzadecorada.preco() + 5
 
+
 class ExtraGrande(PizzaDecorator):
 
     """Define Valor da Pizza Extra Grande"""
 
     def preco(self):
         return self.pizzadecorada.preco() * 1.3
+
 
 class PizzariaDaNonna:
     ingredientes = {'Massa':10, 
